144afcdeafdc5e69f1ecb1972fed27de4c22f345
Kornienko Aleksey <kornienko199004@gmail.com>
2021-12-30 11:31:04 +0300

    2. Каркас шахматной доски [#792 #244889]

--- chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
+++ chess/src/main/java/ru/job4j/chess/firuges/black/BishopBlack.java
@@ -23,32 +23,23 @@ public class BishopBlack implements Figure {
                     String.format("Could not move by diagonal from %s to %s", position, dest)
             );
         }
-        int deltaX = dest.getX() - position.getX();
-        int deltaY = dest.getY() - position.getY();
+        int deltaX = dest.getX() - position.getX() > 0 ? 1 : -1;
+        int deltaY = dest.getY() - position.getY() > 0 ? 1 : -1;
         int size = Math.abs(dest.getX() - position.getX());
+
+        int x = position.getX();
+        int y = position.getY();
         Cell[] steps = new Cell[size];
         for (int index = 0; index < size; index++) {
-            int x = deltaX > 0 ? position.getX() + index + 1 : position.getX() - (index + 1);
-            int y = deltaY > 0 ? position.getY() + index + 1 : position.getY() - (index + 1);
-            steps[index] = Cell.findBy(x, y);
+           x += deltaX;
+           y += deltaY;
+           steps[index] = Cell.findBy(x, y);
         }
         return steps;
     }
 
     public boolean isDiagonal(Cell source, Cell dest) {
-        int x1 = source.getX();
-        int y1 = source.getY();
-        int x2 = dest.getX();
-        int y2 = dest.getY();
-
-        if (x1 == x2 || y1 == y2) {
-            return false;
-        }
-
-        int deltaX = Math.abs(x2 - x1);
-        int deltaY = Math.abs(y2 - y1);
-
-        return deltaX == deltaY;
+        return Math.abs(dest.getX() - source.getX()) == Math.abs(dest.getY() - source.getY());
     }
 
     @Override
--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -1,6 +1,5 @@
 package ru.job4j.chess;
 
-import org.junit.Ignore;
 import org.junit.Test;
 import ru.job4j.chess.firuges.Cell;
 import ru.job4j.chess.firuges.Figure;
@@ -11,15 +10,6 @@ import static org.junit.Assert.assertThat;
 
 public class LogicTest {
 
-    @Ignore
-    @Test
-    public void move()
-            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
-        Logic logic = new Logic();
-        logic.add(new BishopBlack(Cell.C1));
-        logic.move(Cell.C1, Cell.H6);
-    }
-
     @Test
             (expected = OccupiedCellException.class)
     public void moveOnNotFreeCell()
@@ -28,5 +18,22 @@ public class LogicTest {
         logic.add(new BishopBlack(Cell.C1));
         logic.add(new BishopBlack(Cell.H6));
         logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test
+            (expected = FigureNotFoundException.class)
+    public void moveOnEmptyCell()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.move(Cell.C1, Cell.H6);
+    }
+
+    @Test
+            (expected = ImpossibleMoveException.class)
+    public void moveOnWrongWay()
+            throws FigureNotFoundException, OccupiedCellException, ImpossibleMoveException {
+        Logic logic = new Logic();
+        logic.add(new BishopBlack(Cell.C1));
+        logic.move(Cell.C1, Cell.H5);
     }
 }
